0a94d955d394adb9e341671678fe2c2ad364d3f0
Menardo Madiumba <menardotera@gmail.com>
2025-11-09 14:34:31 +0100

    Mise en place des models et enregistrement dans l'admin.py

--- App_facture/admin.py
+++ App_facture/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import * 
+
+class AdminCustomer(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'adress', 'sex', 'age', 'city', 'zip_code', 'created_data', 'save_by')
+
+
+class AdminInvoice(admin.ModelAdmin):
+    list_display = ('customer', 'save_by', 'invoice_date_time', 'total', 'last_update_date', 'paid', 'invoice_type', 'comments')
+
+
+admin.site.register(Customer, AdminCustomer)
+admin.site.register(Invoice, AdminInvoice)
